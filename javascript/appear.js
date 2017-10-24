5993be20cbce740b52e7be741f430772fe224675
382026999 <382026999@qq.com>
2017-10-24 15:04:08 +0800

    first commit

---
+++ javascript/appear.js
@@ -0,0 +1,11 @@
+/**
+ * Created by john on 2017/10/16.
+ */
+var wow = new WOW({
+    boxClass: 'wow',
+    animateClass: 'animated',
+    offset: 0,
+    mobile: true,
+    live: true
+});
+wow.init();
